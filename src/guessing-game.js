7534674df510c4d3e2cf546d82832f9c35e1e2d2
Olga Arkhipenko <arkhipenko.o@MacBook-Pro-Olga.local>
2017-09-17 00:02:29 +0300

    solved

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,29 @@
 class GuessingGame {
-    constructor() {}
+
+    constructor() {
+        this.min = null;
+        this.max = null;
+        this.curVal = null;
+
+    }
 
     setRange(min, max) {
+        this.min = min;
+        this.max = max;
 
     }
 
     guess() {
-
+        this.curVal =  Math.ceil((this.max-this.min)/2 + this.min);
+        return this.curVal;
     }
 
     lower() {
-
+        this.max = Math.ceil((this.max-this.min)/2 + this.min);
     }
 
     greater() {
-
+        this.min = Math.ceil((this.max-this.min)/2 + this.min);
     }
 }
 
